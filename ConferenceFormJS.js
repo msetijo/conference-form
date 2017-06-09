04712d97293cf0e55a3a9596fbd04d1b5bf7192f
Mel S <msetijo@users.noreply.github.com>
2017-06-09 12:12:44 -0400

    Create ConferenceFormJS.js

--- ConferenceFormJS.js
+++ ConferenceFormJS.js
@@ -1,6 +1,3 @@
-</script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>
   window.onload = function() {
     if (window.jQuery) {  
         // jQuery is loaded  
@@ -16,4 +13,4 @@
 //$(".fieldtype-3-0 .class123-radio .label-text .label-cont .class123-labelinfo").text("slots left");
 
 });
-</script>
+
