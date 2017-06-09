4e7fc61901c5fd0150cd79af0ba86214683b815e
Mel S <msetijo@users.noreply.github.com>
2017-06-09 12:06:27 -0400

    Create ConferenceFormJS.js

--- ConferenceFormJS.js
+++ ConferenceFormJS.js
@@ -1,6 +1,15 @@
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
+  window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
   $(document).ready(function() {
 
   alert("ready");
