c4f315788e8f958590aa56846450b2d7efe18850
Mel S <msetijo@users.noreply.github.com>
2017-06-09 12:44:44 -0400

    Create ConferenceFormJS.js

--- ConferenceFormJS.js
+++ ConferenceFormJS.js
@@ -1,7 +1,7 @@
 
   $(document).ready(function() {
 
-  $("#row11 label[title=""] .class123-threecol .class123-radio .label-text .label-cont .class123-labelinfo").text("slots left");
+  $("#row11 label[title=""] .class123-threecol .class123-radio .label-text .label-cont .class123-labelinfo").text().replace("choices left", "slots left");
 
 });
 
