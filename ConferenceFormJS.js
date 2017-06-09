aa1250bbac5da14cbc6a111f0fbc3584522ae7e8
Mel S <msetijo@users.noreply.github.com>
2017-06-09 12:24:34 -0400

    Create ConferenceFormJS.js

--- ConferenceFormJS.js
+++ ConferenceFormJS.js
@@ -1,22 +1,7 @@
-/*var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);*/
 
-  
-window.onload = function() {
-    if (window.jQuery) {  
-        // jQuery is loaded  
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
   $(document).ready(function() {
 
-  alert("ready");
-//$(".fieldtype-3-0 .class123-radio .label-text .label-cont .class123-labelinfo").text("slots left");
+  $(".fieldtype-3-0 .class123-radio .label-text .label-cont .class123-labelinfo").text("slots left");
 
 });
 
