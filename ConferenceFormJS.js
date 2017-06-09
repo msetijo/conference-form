26aa5e904359c99d5b7505059683dabc2c3ef87d
Mel S <msetijo@users.noreply.github.com>
2017-06-09 12:24:08 -0400

    Create ConferenceFormJS.js

--- ConferenceFormJS.js
+++ ConferenceFormJS.js
@@ -1,7 +1,7 @@
-var script = document.createElement('script');
+/*var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
 script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+document.getElementsByTagName('head')[0].appendChild(script);*/
 
   
 window.onload = function() {
