9d109ba6c924e7be9364d40360a528ea54dfcc9e
Mel S <msetijo@users.noreply.github.com>
2017-06-12 11:01:04 -0400

    Create ConferenceFormJS.js

--- ConferenceFormJS.js
+++ ConferenceFormJS.js
@@ -1,6 +1,14 @@
 
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/ui/1.12.1/jquery-ui.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
   $(document).ready(function() {
 
+    //Changes 'choice left' words
   $(".fieldcontainer .fieldtype-3-0 .rowright label:not([title]) .class123-labelinfo").text(function () {
     return $(this).text().replace("choices left", "slots left").replace("choice left", "slot left");
   });
@@ -8,7 +16,11 @@
       return $(this).text().replace("0 choices left", "session is full");
   });
   
+    //removes automatic selection when user clicks on time label
   $(".fieldcontainer .fieldtype-3-0 .rowleft label").removeAttr("for");
 
+  $( function() {
+    $( document ).tooltip();
+  } );
 });
 
